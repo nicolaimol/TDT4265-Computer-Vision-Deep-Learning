2f0adc81bcdcb982b32915368ce2f85c011b138b
mbirkhez <mamoonashami@live.com>
2023-01-25 13:20:11 +0100

    Update create_submission_zip.py

--- assignment1/create_submission_zip.py
+++ assignment1/create_submission_zip.py
@@ -26,7 +26,7 @@ files_to_include = {
     "task3a": [".py"],
     "trainer": [".py"],
     "utils": [".py"],
-    "mnist.py": [".py"],
+    "mnist": [".py"],
 }
 zipfile_path = "assignment_code.zip"
 print("-"*80)
